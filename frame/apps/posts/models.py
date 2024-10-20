f16b6323d72ff2175e907762160ef1af9d4e7c6c
Carlos Eduardo <ducameneses16@gmail.com>
2024-10-19 21:52:37 -0300

    feat: Init project

---
+++ frame/apps/posts/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.URLField(max_length=500)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(
+        max_length=100,
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.title}'
---
+++ frame/apps/posts/views.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.forms import ModelForm
+from django.shortcuts import redirect, render
+
+from .models import Post
+
+
+def home_view(request):
+    posts = Post.objects.all()
+    return render(request, 'apps/posts/home.html', {'posts': posts})
+
+
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        labels = {
+            'body': 'Caption',
+        }
+        widgets = {
+            'body': forms.Textarea(
+                attrs={
+                    'rows': 3,
+                    'placeholder': 'Add a caption ...',
+                    'class': 'font1 text-4xl'
+                }
+            ),
+        }
+
+
+def post_create_view(request):
+    form = PostCreateForm()
+
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'apps/posts/post_create.html', {'form': form})
